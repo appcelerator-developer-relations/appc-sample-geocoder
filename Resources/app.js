72944d33061037698d68c67fdcf8439e4c90709f
Tony Lukasavage <anthony.lukasavage@gmail.com>
2012-01-12 15:08:30 -0500

    enforcing 1.8+ and no mobile web (for now)

--- Resources/app.js
+++ Resources/app.js
@@ -1,6 +1,14 @@
 // This is a single context, single window application
 // There is only one master window to which sub views will be added
 (function() {
-	var ApplicationWindow = require('ui/ApplicationWindow').ApplicationWindow;
-	new ApplicationWindow().open();
+	if (Ti.version < 1.8 ) {
+		alert('Sorry - this application requires Titanium Mobile SDK 1.8 or later');
+	}
+	else if (Ti.Platform.osname === 'mobileweb') {
+		alert('Mobile web is not yet supported by this template');
+	}
+	else {
+		var ApplicationWindow = require('ui/ApplicationWindow').ApplicationWindow;
+		new ApplicationWindow().open();
+	}
 })();

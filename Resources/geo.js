5981b5053a9767093d57dbeb36c78edf7b133857
tonylukasavage <anthony.lukasavage@gmail.com>
2012-03-21 10:24:39 -0400

    using client-side forward geocoding for mobileweb

--- Resources/geo.js
+++ Resources/geo.js
@@ -1,8 +1,25 @@
 var GOOGLE_BASE_URL = 'http://maps.googleapis.com/maps/geo?output=json&q=';
+var ERROR_MESSAGE = 'There was an error geocoding. Please try again.';
 exports.LATITUDE_BASE = 37.389569;
 exports.LONGITUDE_BASE = -122.050212;
 
+var GeoData = function(title, latitude, longitude) {
+	this.title = title;
+	this.coords = {
+		latitude: latitude,
+		longitude: longitude
+	};
+};
+
 exports.forwardGeocode = function(address, callback) {
+	if (Ti.Platform.osname === 'mobileweb') {
+		forwardGeocodeWeb(address, callback);
+	} else {
+		forwardGeocodeNative(address, callback);
+	}
+};
+
+var forwardGeocodeNative = function(address, callback) {
 	var xhr = Titanium.Network.createHTTPClient();
 	xhr.open('GET', GOOGLE_BASE_URL + address);
 	xhr.onload = function() {
@@ -12,14 +29,35 @@ exports.forwardGeocode = function(address, callback) {
 	    	return;
 	    }
 	    
-	    var point = {
-	    	latitude: json.Placemark[0].Point.coordinates[1],
-	    	longitude: json.Placemark[0].Point.coordinates[0]
-	    };
-	    callback({
-    		title: address,
-    		coords: point
-		});
+	    callback(new GeoData(
+	    	address,
+	    	json.Placemark[0].Point.coordinates[1],
+	    	json.Placemark[0].Point.coordinates[0]
+	    ));
+	};
+	xhr.onerror = function(e) {
+		Ti.API.error(e.error);
+		alert(ERROR_MESSAGE);
 	};
 	xhr.send();
+};
+
+var forwardGeocodeWeb = function(address, callback) {
+	var geocoder = new google.maps.Geocoder();
+	if (geocoder) {
+      	geocoder.geocode({ 'address': address }, function (results, status) {
+        	if (status == google.maps.GeocoderStatus.OK) {
+        		callback(new GeoData(
+        			address, 
+        			results[0].geometry.location.Ua,
+        			results[0].geometry.location.Va
+        		));
+         	} else {
+         		Ti.API.error(status);
+         		alert(ERROR_MESSAGE); 	
+         	}
+      	});
+	} else {
+		alert('Google Maps Geocoder not supported');	
+	}
 };
--- Resources/ui/ApplicationWindow.js
+++ Resources/ui/ApplicationWindow.js
@@ -71,8 +71,8 @@ exports.ApplicationWindow = function() {
 		
 		// Handle all map annotation clicks
 		mapview.addEventListener('click', function(e) {
-			Ti.API.info('clicked map');
-			Ti.API.info(e.clicksource);
+			//Ti.API.info('clicked map');
+			//Ti.API.info(e.clicksource);
 	        if (e.annotation && (e.clicksource === 'leftButton' || e.clicksource == 'leftPane')) {    
 	            mapview.removeAnnotation(e.annotation);
 	        }         
@@ -84,6 +84,9 @@ exports.ApplicationWindow = function() {
 	button.addEventListener('click', function() {	
 		textfield.blur();
 		geo.forwardGeocode(textfield.value, function(geodata) {
+			// Ti.API.info(geodata.title);
+			// Ti.API.info(geodata.coords.latitude);
+			// Ti.API.info(geodata.coords.longitude);
 			mapview.addAnnotation(Ti.Map.createAnnotation({
 		    	animate: true,
 		    	pincolor: Titanium.Map.ANNOTATION_RED,

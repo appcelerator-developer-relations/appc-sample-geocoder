12ddec50d4281585c63a783a174c2ca5b6543cc3
tonylukasavage <anthony.lukasavage@gmail.com>
2012-03-21 08:50:04 -0400

    allow mobileweb

--- Resources/app.js
+++ Resources/app.js
@@ -4,9 +4,6 @@
 	if (Ti.version < 1.8 ) {
 		alert('Sorry - this application requires Titanium Mobile SDK 1.8 or later');
 	}
-	else if (Ti.Platform.osname === 'mobileweb') {
-		alert('Mobile web is not yet supported by this template');
-	}
 	else {
 		var ApplicationWindow = require('ui/ApplicationWindow').ApplicationWindow;
 		new ApplicationWindow().open();

0dea445cc631b9bcf1d551dfd73627a868d33d93
euforic <cs@euforic.co>
2013-10-11 16:32:23 -0700

    [TIMOB-14806]

--- Resources/app.js
+++ Resources/app.js
@@ -1,10 +1,9 @@
 // This is a single context, single window application
 // There is only one master window to which sub views will be added
 (function() {
-	if (Ti.version < 1.8 ) {
+	if (Ti.version < 1.8) {
 		alert('Sorry - this application requires Titanium Mobile SDK 1.8 or later');
-	}
-	else {
+	} else {
 		var ApplicationWindow = require('ui/ApplicationWindow').ApplicationWindow;
 		new ApplicationWindow().open();
 	}
